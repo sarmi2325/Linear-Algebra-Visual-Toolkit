168427f76b308445f013a79ab7b79ae7c66fec17
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-07-15 01:56:36 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -3,33 +3,44 @@ import streamlit as st
 import sympy as sp
 
 def gaussian_elimination(A, b):
-    A = A.astype(float)
+    #conveting to float for better division
+    A = A.astype(float)   
     b = b.astype(float)
+    #assigning row and column
     m, n = A.shape
+    #if not a square matrix, cannot solve
     if m != n:
         raise ValueError("Only square systems (m = n) can be solved.")
-
+    #loop through each column - creating a row echelon form matrix (upper triangular matrix)
     for i in range(n):
+        #finding max absolute value in each columns below the pivot
         max_row = np.argmax(abs(A[i:, i])) + i
+        #checking if max of abs of the selected column is zero or not. if zero, we cannot reduce further
         if A[max_row, i] == 0:
             raise ValueError("Matrix is singular or no unique solution.")
+        #keep the largest pivot row value at the top
         A[[i, max_row]] = A[[max_row, i]]
         b[[i, max_row]] = b[[max_row, i]]
+        #eliminate the values under the pivot make it zero
         for j in range(i+1, n):
+            #divide each value in column i with the pivot
             factor = A[j, i] / A[i, i]
             A[j, i:] -= factor * A[i, i:]
             b[j] -= factor * b[i]
-
+    #for holding solution 
     x = np.zeros(n)
+    #moving the loop from bottom
     for i in range(n-1, -1, -1):
+        #back substitution
         x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
     return x
 
 def linear_equation_solver():
     st.title("Linear Equation Solver with Gaussian Elimination")
+    #rows and columns
     m = st.number_input("Number of Equations (m)", min_value=1, max_value=10, value=3)
     n = st.number_input("Number of Unknowns (n)", min_value=1, max_value=10, value=3)
-
+    #coefficient matrix
     A = []
     st.markdown("### Coefficient Matrix A")
     for i in range(m):
@@ -40,7 +51,7 @@ def linear_equation_solver():
             row.append(val)
         A.append(row)
     A = np.array(A)
-
+    #right hand vector
     b = []
     st.markdown("### Right-Hand Side Vector b")
     cols_b = st.columns(m)
